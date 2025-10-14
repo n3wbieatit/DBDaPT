2d4567184db257bb4ca6bebb58dba12c25cf45f1
Mikhail Lesnikov <lesnikovmisch@yandex.ru>
2025-10-14 23:11:13 +0500

    feat: Add connection and disconnection to DB

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import { buildApp } from './app';
+import { connectDB, disconnectDB } from './db/prisma';
 
 // Задание порта приложения из .env файла. Если порт не задан, то используется порт 3000
 const port = Number(process.env.PORT ?? 3000);
 const app = buildApp();
 
-// Запуск приложения
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+// Функция запуска приложения
+async function start() {
+  // Подключение к базе данных
+  await connectDB();
+  // Затем запуск сервера
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
+}
+
+// Запуск приложения с уловом ошибки
+start().catch(async (error) => {
+  console.error('Error starting server:', error);
+  await disconnectDB();
+  // Обозначение базового завершения
+  process.exit(1);
 });
+
+// Функция аварийной остановки приложения
+async function stop() {
+  await disconnectDB();
+  // Обозначение аварийной остановки
+  process.exit(0);
+}
+
+// Обозначения в случае аварийной остановки
+process.on('SIGINT', stop);
+process.on('SIGTERM', stop);
